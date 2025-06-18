132084e52adf499823fa186ba3d547b88712fdc2
Abd-Sobr <66172515+Abdoul-lab@users.noreply.github.com>
2025-06-18 17:08:26 +0000

    config deployement

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/rv-7immig/',
+  build: {
+    chunkSizeWarningLimit: 1000,} // en kilo-octets, ici 1000 kB = 1MB
 })
